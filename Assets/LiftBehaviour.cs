396850d7632b0709568e9ae6b98d7537ca8fd321
Kristián Žuffa <kristianzuffa@MacBook-Pro-uzivatela-Kristian.local>
2018-12-03 18:02:17 +0100

    init

---
+++ Assets/LiftBehaviour.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class LiftBehaviour : MonoBehaviour
+{
+    private readonly int _liftSpeed = 3;
+    private bool _ptrOnButton;
+    private bool _lifting;
+    private int _phase;
+    private Vector3? _finalPos;
+
+    public void OnEnter()
+    {
+        _ptrOnButton = true;
+    }
+
+    public void OnLeave()
+    {
+        _ptrOnButton = false;
+    }
+
+    private void AnimateTo(string gameObject, Vector3 finalPositionAddition, bool resetPhase = false)
+    {
+        var obj = GameObject.Find(gameObject);
+        if (_finalPos == null)
+        {
+            _finalPos = obj.transform.position + finalPositionAddition;
+        }
+
+        float step = _liftSpeed * Time.deltaTime;
+        obj.transform.position = Vector3.MoveTowards(obj.transform.position, (Vector3) _finalPos, step);
+        if (obj.transform.position == _finalPos)
+        {
+            _phase++;
+            _finalPos = null;
+            if (resetPhase)
+            {
+                _phase = 0;
+                _lifting = false;
+            }
+        }
+    }
+
+    public void Update()
+    {
+        if (_ptrOnButton && Input.inputString.Length > 0 && Input.inputString[0] == 10 && !_lifting)
+        {
+            _lifting = true;
+        }
+
+        if (_lifting)
+        {
+            if (_phase == 0)
+            {
+                AnimateTo("HorizontalLift", Vector3.up * 1.4f);
+            }
+            else if (_phase == 1)
+            {
+                AnimateTo("Pusher", Vector3.forward * 2f);
+            }
+            else if (_phase == 2)
+            {
+                AnimateTo("Pusher", -Vector3.forward * 2f);
+            }
+            else
+            {
+                AnimateTo("HorizontalLift", -Vector3.up * 1.4f, true);
+            }
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Camera _cam;
+    private CharacterController _controller;
+    private const float Speed = 60f;
+    private const float JumpSpeed = 8.0f;
+    private const float Gravity = 20.0f;
+    private Vector3 _moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        _cam = Camera.main;
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (_controller.isGrounded)
+        {
+            _moveDirection = (_cam.transform.position - _controller.transform.position) * Input.GetAxis("Vertical");
+            _moveDirection = transform.TransformDirection(_moveDirection);
+            _moveDirection = _moveDirection * Speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                _moveDirection.y = JumpSpeed;
+            }
+        }
+
+        _moveDirection.y = _moveDirection.y - Gravity * Time.deltaTime;
+        _controller.Move(_moveDirection * Time.deltaTime);
+    }
+}
